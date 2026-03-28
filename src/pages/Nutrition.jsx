9af6df2803cbde6dbdfcf2d241d9ea5a922f6c64
scarface57000 <141498965+scarface57000@users.noreply.github.com>
2026-03-28 06:20:47 +0100

    Create Nutrition component for nutrition and calorie tracking

---
+++ src/pages/Nutrition.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nutrition = () => {
+    return (
+        <div>
+            <h1>Nutrition and Calorie Tracker</h1>
+            <p>This page will help you track your nutritional intake and calories.</p>
+        </div>
+    );
+};
+
+export default Nutrition;
