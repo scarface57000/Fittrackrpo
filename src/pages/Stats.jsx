e29da40256ef91f0fb2f278f1add9e0815306385
scarface57000 <141498965+scarface57000@users.noreply.github.com>
2026-03-28 06:20:38 +0100

    Add Stats component for statistics and progress visualization

---
+++ src/pages/Stats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Stats = () => {
+    return (
+        <div>
+            <h1>Statistics and Progress Visualization</h1>
+            {/* Add your chart components here */}
+        </div>
+    );
+};
+
+export default Stats;
