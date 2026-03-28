f9445e353fe977a40f15b809e88c876b83b9cd5d
scarface57000 <141498965+scarface57000@users.noreply.github.com>
2026-03-28 06:20:43 +0100

    Create Settings component for user profile and app settings

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>User Profile and App Settings</h1>
+      {/* Add user profile information and settings form here */}
+    </div>
+  );
+};
+
+export default Settings;
