8293da634e5390ee4a34ecaa57d79b1c4a4f987c
scarface57000 <141498965+scarface57000@users.noreply.github.com>
2026-03-28 06:36:48 +0100

    Updated vite.config.js to use React plugin instead of Vue plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {}
-    })
-  ]
+  plugins: [react()],
 });
